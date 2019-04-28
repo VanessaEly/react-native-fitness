f7e46d42373a1effff74d7b9dd0ccd6d55cf1a05
Vanessa Ely <vah_ely@hotmail.com>
2019-04-28 00:47:05 -0300

    feat: added utils

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// this library is added by default to create-react-native-app
+import { Ionicons } from '@expo/vector-icons';
 
+/**
+ *  Expo is a set of tools and services that allow us to build native (iOS and Android) applications with JavaScript
+ *  Expo makes it easy to build mobile applications without having to write native code (e.g. Swift, Objective C, Java)
+ */
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! bla</Text>
+        {
+          /** Texts always needs to be written inside of the <Text> native component, or it'll cause 
+            a render error.
+            Icons -> Create-react-native app comes with Vector Icons by default.
+            Directory of icons available can be found at https://expo.github.io/vector-icons/
+          */
+        }
+        <Ionicons name='ios-pizza' color='red' size={100} />
+        <Text>Open up App.js to start working on your app! Test</Text>
       </View>
     );
   }
