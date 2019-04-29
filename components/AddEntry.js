e2a9388fa056ed824861f20f86cc352c8c9b87b2
Vanessa Ely <vah_ely@hotmail.com>
2019-04-28 21:39:09 -0300

    feat: added button basic functionalities, missing layout

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers';
-import Slider from './Slider';
-import Steppers from './Steppers';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
+import FitnessSlider from './FitnessSlider';
+import FitnessSteppers from './FitnessSteppers';
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
+
+const SubmitBtn = ({ onPress }) => {
+  return (
+    /**
+     * TouchableOpacity is one of the react-native's components that handles 'tapping gestures',
+     * also called 'Touchables'.
+     * Available handlers are: Button, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback,
+     * and TouchableWithoutFeedback
+     */
+    <TouchableOpacity
+      onPress={onPress}
+    >
+      <Text>Submit</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -27,7 +45,6 @@ export default class AddEntry extends Component {
     })
   }
   decrement = (metric) => {
-
     this.setState((state) => {
       // adding the object step (got by getMetricMetaInfo) to the current value
       const count = state[metric] - getMetricMetaInfo(metric).step;
@@ -43,8 +60,40 @@ export default class AddEntry extends Component {
       [metric]: value,
     }));
   }
+  submit = () => {
+    const key = timeToString();
+    const entry = this.state;
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }));
+  }
+  reset = () => {
+    const key = timeToString();
+
+  }
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          {
+            /** 
+              Icons -> Create-react-native app comes with Vector Icons by default.
+              Directory of icons available can be found at https://expo.github.io/vector-icons/
+            */
+          }
+          <Entypo name='emoji-happy' size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
@@ -53,6 +102,7 @@ export default class AddEntry extends Component {
           // it to the DateHeader component
         }
         <DateHeader date={(new Date()).toLocaleDateString()} />
+        <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
@@ -61,12 +111,12 @@ export default class AddEntry extends Component {
             <View key={key}>
               {getIcon()}
               {type === 'slider'
-                ? <Slider
+                ? <FitnessSlider
                     value={value}
                     onChange={(value) => this.slide(key, value)}
                     {...rest}
                   />
-                : <Steppers
+                : <FitnessSteppers
                     value={value}
                     onIncrement={() => this.increment(key)}
                     onDecrement={() => this.decrement(key)}
@@ -76,6 +126,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     );
   }
--- components/DateHeader.js
+++ components/DateHeader.js
@@ -3,6 +3,8 @@ import { Text } from 'react-native';
 
 export default DateHeader = ({ date }) => {
   return (
+    // Texts always needs to be written inside of the <Text> native component, or it'll cause 
+    // a render error.
     <Text>
       {date}
     </Text>
