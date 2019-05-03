6bff8fa0d5dc031dcc8727adfae46a87bc73f52a
Vanessa Ely <vah_ely@hotmail.com>
2019-05-03 01:59:55 -0300

    chore: added slider, steppers, textbutton and dateHeader styles

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { purple } from '../utils/colors';
 
 export default DateHeader = ({ date }) => {
   return (
     // Texts always needs to be written inside of the <Text> native component, or it'll cause 
     // a render error.
-    <Text>
+    <Text style={{color: purple, fontSize: 25}}>
       {date}
     </Text>
   );
--- components/FitnessSteppers.js
+++ components/FitnessSteppers.js
@@ -1,22 +1,74 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { white, gray, purple } from '../utils/colors';
 
 export default Steppers = ({ max, unit, step, value, onIncrement, onDecrement }) => {
   return (
-    <View>
-      <View>
-        <TouchableOpacity onPress={onDecrement}>
-          <FontAwesome name='minus' size={30} color='black' />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onIncrement}>
-          <FontAwesome name='plus' size={30} color='black' />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+    <View style={[styles.row, {justifyContent: 'space-between'}]}>
+      {Platform.OS === 'ios'
+        ? <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+            // TouchableOpacity is used to give a opaque effect when the button is touched (or clicked)
+            // bordertopRightRadius 0 is used to remove the redius of only this specific side
+            style={[styles.iosBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}
+            onPress={onDecrement}>
+              <Entypo name='minus' size={30} color={purple} />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={[styles.iosBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}
+            onPress={onIncrement}>
+              <Entypo name='plus' size={30} color={purple} />
+            </TouchableOpacity>
+          </View>
+        : <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+          // TouchableOpacity is used to give a opaque effect when the button is touched (or clicked)
+          // bordertopRightRadius 0 is used to remove the redius of only this specific side
+          style={[styles.androidBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}
+          onPress={onDecrement}>
+            <FontAwesome name='minus' size={30} color={white} />
+          </TouchableOpacity>
+          <TouchableOpacity
+          style={[styles.androidBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}
+          onPress={onIncrement}>
+            <FontAwesome name='plus' size={30} color={white} />
+          </TouchableOpacity>
+        </View>
+      }
+      <View style={styles.metricCounter}>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+        <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row', // horizontal main axis
+    flex: 1,
+    alignItems: 'center', // aligning cross axis (vertical) center
+  },
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  androidBtn: {
+    margin: 5,
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 2,
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }
+});
